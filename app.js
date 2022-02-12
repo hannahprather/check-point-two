503ca6b43eea247491cd1bcf24886ec797929c8b
Hannah Prather <prather.hannah.m@gamil.com>
2022-02-11 17:16:32 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+let clickUpgrades = {
+  Moisture: {
+    price: 50,
+    quantity: 0,
+    multiplier: 2
+  },
+  C3P0: {
+    price: 200,
+    quantity: 0,
+    multiplier: 3
+  },
+  Bantha: {
+    price: 500,
+    quantity: 0,
+    multiplier: 5
+  }
+};
+
+
+let automaticUpgrades = {
+  theForce: {
+    price: 999,
+    quantity: 0,
+    multiplier: 20
+  }
+};
+
+
+
+// function draw() {
+//   let template = ''
+//   for (let key in clickUpgrades) {
+//     let cUpgrade = clickUpgrades[key]
+//     template += `
+//       <div id='upgrade'>
+//         <p><button type="button" class="btn btn-outline-light"> ${key}</button> 2x weenies <span
+//             class="fw-b"> 0 </span>
+//             </p>
+//       </div>
+//         `
+//     document.getElementById('upgrade').innerHTML = template
+//   }
+// }
+
+// // function draw ()
+
+
+
+
+
+// draw()
+
+functions count(){
+
+}
